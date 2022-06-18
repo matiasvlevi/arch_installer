c1d99df3e619b535cbd81103b5579d66b043bced
matiasvlevi <matiasvlevi@gmail.com>
2022-06-18 23:10:09 +0000

    init build

--- src/packages.rs
+++ src/packages.rs
@@ -1,13 +1,15 @@
 pub use std::process::{Command, Stdio};
 
 pub fn pacstrap(packages: Vec<&str>) {
-    let mut pacstrap_cmd = Command::new("pacstrap")
+    let pacstrap_cmd = Command::new("pacstrap")
         .arg("/mnt")
         .args(packages)
         .arg("--noconfirm")
         .stdout(Stdio::piped())
         .output()
         .unwrap();
+
+    println!("status: {}", pacstrap_cmd.status);
 
     return;
 }
